6cafd07a317a5ccd8da8d7ad79b1cab1701d3e3c
katsuhisa91 <91katsuhisa@gmail.com>
2022-10-20 17:00:55 +1100

    Initial commit

---
+++ script.py
@@ -0,0 +1,13 @@
+import sys
+print (sys.version)
+print (sys.executable)
+
+def greet(who_to_greet):
+    greeting ='hello, {}'.format(who_to_greet)
+    return greeting
+
+print(greet ('World'))
+print(greet('Ham1122'))
+
+name = input("yourname?")
+print("hello" , name) 
